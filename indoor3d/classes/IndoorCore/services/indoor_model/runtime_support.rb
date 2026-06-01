1b096c07a6189d6784cfa92b2436998af6002519
ulol <jeffk0408@naver.com>
2026-06-01 10:03:27 +0900

    Split IndoorModel coordinator modules

---
+++ indoor3d/classes/IndoorCore/services/indoor_model/runtime_support.rb
@@ -0,0 +1,163 @@
+# frozen_string_literal: true
+
+module ULOL
+  module Indoor3DGmlModeler
+    module IndoorCore
+      class IndoorModel
+        module RuntimeSupport
+          def refresh_runtime_data
+            return true if @refreshing_runtime
+
+            @refreshing_runtime = true
+            sync do
+              @model = Sketchup.active_model
+              find_existing_space_features_groups
+              attach_existing_space_features_observers
+              reset_runtime_collections
+              @runtime_restorer.restore(primal_group: @primal_group, dual_group: @dual_group)
+            end
+            puts "[IndoorGML] Runtime refreshed: cells=#{@cell_spaces.length}, states=#{@states.length}, transitions=#{@transitions.length}"
+            true
+          ensure
+            @refreshing_runtime = false
+          end
+
+          private
+
+          def bind_registry_collections
+            @cell_spaces = @feature_registry.cell_spaces
+            @states = @feature_registry.states
+            @transitions = @feature_registry.transitions
+            @doors = @feature_registry.doors
+            @transfer_spaces = @feature_registry.transfer_spaces
+            @adjacent_cell_space_pairs = @feature_registry.adjacent_cell_space_pairs
+            @transitions_by_cell_pair = @feature_registry.transitions_by_cell_pair
+          end
+
+          def reset_runtime_collections
+            @feature_registry.reset!
+            bind_registry_collections
+          end
+
+          def stale_cell_space_runtime?(cell_space, entity)
+            return true if cell_space.nil?
+            return true unless cell_space.valid?
+            return true unless cell_space.sketchup_group == entity
+            return true unless cell_space.duality_state&.valid?
+
+            false
+          rescue StandardError
+            true
+          end
+
+          def stale_state_runtime?(state, entity)
+            return true if state.nil?
+            return true unless state.valid?
+            return true unless state.sketchup_component_instance == entity
+            return true unless state.duality_cell&.valid?
+
+            false
+          rescue StandardError
+            true
+          end
+
+          def refresh_and_find_cell_space(entity)
+            refresh_runtime_data
+            find_cell_space_for_entity(entity)
+          end
+
+          def refresh_and_find_state(entity)
+            refresh_runtime_data
+            find_state_for_entity(entity)
+          end
+
+          def write_space_features_attributes(group, feature)
+            @attribute_serializer.write_space_features(group, feature)
+          end
+
+          def write_attributes(cell_space)
+            @attribute_serializer.write_cell_space_and_state(cell_space)
+            lock_indoor_entity(cell_space.sketchup_group)
+            lock_indoor_entity(cell_space.duality_state.sketchup_component_instance)
+          end
+
+          def write_cell_space_attributes(cell_space)
+            @attribute_serializer.write_cell_space(cell_space)
+            lock_indoor_entity(cell_space.sketchup_group)
+          end
+
+          def write_state_attributes(state)
+            @attribute_serializer.write_state(state)
+            lock_indoor_entity(state.sketchup_component_instance) if state&.valid?
+          end
+
+          def write_transition_attributes(transition)
+            @attribute_serializer.write_transition(transition)
+            lock_indoor_entity(transition.edge) if transition&.edge&.valid?
+          end
+
+          def indoor_gml_entity?(entity)
+            @attribute_serializer.indoor_gml_entity?(entity)
+          end
+
+          def indoor_attribute(entity, key)
+            @attribute_serializer.attribute(entity, key)
+          end
+
+          def indoor_feature(entity)
+            @attribute_serializer.feature(entity)
+          end
+
+          def dual_feature?(entity)
+            ['State', 'Transition'].include?(indoor_feature(entity))
+          end
+
+          def space_features_feature?(feature)
+            feature == PRIMAL_GROUP_FEATURE || feature == DUAL_GROUP_FEATURE
+          end
+
+          def converted_group?(sketchup_group)
+            @attribute_serializer.converted_group?(sketchup_group)
+          end
+
+          def find_cell_space_for_entity(entity)
+            @feature_registry.find_cell_space_for_entity(entity)
+          end
+
+          def find_state_for_entity(entity)
+            @feature_registry.find_state_for_entity(entity)
+          end
+
+          def with_unlocked(_entity)
+            yield
+          end
+
+          def lock_indoor_entity(_entity)
+            # Lock policy is intentionally deferred until observer behavior is easier to test.
+            true
+          end
+
+          def unlock_indoor_entity(entity)
+            entity.locked = false if entity&.valid? && entity.respond_to?(:locked=)
+          rescue StandardError
+            true
+          end
+
+          def sync
+            @syncing = true
+            yield
+          ensure
+            @syncing = false
+          end
+
+          def erase_guard
+            @erasing = true
+            yield
+          ensure
+            @erasing = false
+          end
+        end
+      end
+    end
+  end
+end
---
+++ indoor3d/classes/IndoorCore/services/indoor_model/topology.rb
@@ -0,0 +1,142 @@
+# frozen_string_literal: true
+
+module ULOL
+  module Indoor3DGmlModeler
+    module IndoorCore
+      class IndoorModel
+        module Topology
+          def update_transitions_for_state(state)
+            return if state.nil?
+
+            @transitions.each do |transition|
+              next unless transition.connected_to?(state)
+
+              write_transition_attributes(transition) if update_transition(transition)
+            end
+          end
+
+          private
+
+          def connect_states(state1, state2)
+            ensure_space_features_groups
+
+            cell1 = state1&.duality_cell
+            cell2 = state2&.duality_cell
+            return nil if cell1.nil? || cell2.nil?
+
+            create_or_update_transition_for_pair(cell1, cell2)
+          end
+
+          def synchronize_adjacency_and_transitions_for_cell_space(cell_space)
+            @adjacency_service.synchronize_for(cell_space)
+          end
+
+          def erase_adjacency_for_cell_space(cell_space)
+            @adjacency_service.erase_for(cell_space)
+          end
+
+          def create_or_update_transition_for_pair(cell1, cell2)
+            return nil if cell1.nil? || cell2.nil?
+            return nil if cell1 == cell2
+            return nil unless cell1.valid? && cell2.valid?
+            return nil unless cell1.duality_state&.valid? && cell2.duality_state&.valid?
+
+            pair_key = cell_pair_key(cell1, cell2)
+            transition = @feature_registry.transition_for_pair(pair_key)
+            unless transition&.valid?
+              transition = Transition.new(
+                cell1.duality_state,
+                cell2.duality_state,
+                @dual_group.entities,
+                cell1: cell1,
+                cell2: cell2
+              )
+              @feature_registry.add_transition(transition, pair_key: pair_key)
+            end
+
+            return nil unless update_transition(transition)
+
+            register_transition_with_states(transition)
+            register_transition_entity(transition)
+            write_transition_attributes(transition)
+            lock_indoor_entity(transition.edge)
+            transition
+          end
+
+          def erase_transition_for_pair_key(pair_key)
+            transition = @feature_registry.delete_transition_for_pair(pair_key)
+            return if transition.nil?
+
+            erase_transition(transition)
+            @feature_registry.delete_adjacent_pair(pair_key)
+          end
+
+          def erase_transition(transition)
+            return if transition.nil?
+
+            unregister_transition_entity(transition)
+            unregister_transition_from_states(transition)
+            unlock_indoor_entity(transition.edge)
+            transition.erase!
+            @feature_registry.remove_transition(transition)
+          end
+
+          def cell_pair_key(cell1, cell2)
+            @adjacency_service.cell_pair_key(cell1, cell2)
+          end
+
+          def register_transition_with_states(transition)
+            transition.state1.add_transition(transition)
+            transition.state2.add_transition(transition)
+            write_state_attributes(transition.state1)
+            write_state_attributes(transition.state2)
+          end
+
+          def register_transition_entity(transition)
+            return unless transition&.edge&.valid?
+
+            @feature_registry.register_transition_entity(transition)
+          end
+
+          def unregister_transition_entity(transition)
+            return if transition.nil?
+
+            @feature_registry.unregister_transition_entity(transition)
+          end
+
+          def unregister_transition_from_states(transition)
+            transition.state1.remove_transition(transition) if transition.state1
+            transition.state2.remove_transition(transition) if transition.state2
+            write_state_attributes(transition.state1) if transition.state1&.valid?
+            write_state_attributes(transition.state2) if transition.state2&.valid?
+          end
+
+          def erase_transitions_for_state(state)
+            return if state.nil?
+
+            @transitions.delete_if do |transition|
+              next false unless transition.connected_to?(state)
+
+              if transition.cell1 && transition.cell2
+                pair_key = cell_pair_key(transition.cell1, transition.cell2)
+                @feature_registry.delete_transition_for_pair(pair_key)
+                @feature_registry.delete_adjacent_pair(pair_key)
+              end
+              erase_transition(transition)
+              true
+            end
+          end
+
+          def update_transition(transition)
+            with_unlocked(transition.edge) do
+              transition.update(
+                state_local_position(transition.state1),
+                state_local_position(transition.state2)
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
