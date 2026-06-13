e2b5403180d435b2451568ae713533e8e03f73e7
ULOL <jeffk0408@naver.com>
2026-06-13 13:46:51 +0900

    refactor: extract editor session batch progress

--- indoor3d/infrastructure/scene/editor_session.rb
+++ indoor3d/infrastructure/scene/editor_session.rb
@@ -6,7 +6,9 @@ module ULOL
 
       class EditorSession
         require_relative 'editor_session/lock_policy'
+        require_relative 'editor_session/batch_progress'
         include LockPolicy
+        include BatchProgress
 
         GRAPH_VISIBLE_ATTRIBUTE = 'graph_visible'
         GEOMETRY_VISIBLE_ATTRIBUTE = 'geometry_visible'
@@ -68,90 +70,6 @@ module ULOL
         def apply_display_state
           set_dual_overlay_visible(dual_overlay_visible?)
           apply_geometry_visibility()
-        end
-
-        def progress_active?
-          @progress && @progress[:active] == true
-        end
-
-        def progress_current
-          @progress ? @progress[:current].to_i : 0
-        end
-
-        def progress_total
-          @progress ? @progress[:total].to_i : 0
-        end
-
-        def progress_message
-          @progress ? @progress[:message].to_s : ''
-        end
-
-        def start_progress(total, message)
-          @progress = {
-            active: true,
-            current: 0,
-            total: [total.to_i, 0].max,
-            message: message.to_s
-          }
-          model = Sketchup.active_model()
-          ensure_overlay_registered(model)
-          update_overlay_enabled()
-          invalidate_view(model)
-          true
-        end
-
-        def update_progress(current, message = nil)
-          return false unless @progress
-
-          @progress[:current] = [current.to_i, 0].max
-          @progress[:message] = message.to_s if message
-          invalidate_view(Sketchup.active_model())
-          true
-        end
-
-        def finish_progress
-          @progress = nil
-          model = Sketchup.active_model()
-          update_overlay_enabled()
-          invalidate_view(model)
-          true
-        end
-
-        def run_batched(items, message:, batch_size: 20, complete: nil, failure: nil, &block)
-          items = Array(items)
-          return false if items.empty?
-
-          batch_size = [batch_size.to_i, 1].max
-          index = 0
-          total = items.length
-          start_progress(total, message)
-
-          processor = nil
-          processor = proc do
-            begin
-              limit = [index + batch_size, total].min
-              while index < limit
-                block.call(items[index], index) if block
-                index += 1
-              end
-              update_progress(index, message)
-              if index < total
-                UI.start_timer(0, false) { processor.call }
-              else
-                finish_progress()
-                complete&.call()
-              end
-            rescue StandardError => e
-              finish_progress()
-              if failure
-                failure.call(e)
-              else
-                IndoorCore::Logger.puts "[IndoorGML] Batched operation failed: #{e.class}: #{e.message}"
-              end
-            end
-          end
-          UI.start_timer(0, false) { processor.call }
-          true
         end
 
         def begin_editing
