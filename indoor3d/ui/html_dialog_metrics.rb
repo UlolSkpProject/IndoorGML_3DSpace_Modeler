25fa23049ca1cedf28124cb64238eb0b7161cab0
ULOL <jeffk0408@naver.com>
2026-07-08 23:44:37 +0900

    Share HTML dialog chrome height metric

---
+++ indoor3d/ui/html_dialog_metrics.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ULOL
+  module Indoor3DGmlModeler
+    module IndoorCore
+      module HtmlDialogMetrics
+        WINDOW_CHROME_HEIGHT = 44
+      end
+    end
+  end
+end
