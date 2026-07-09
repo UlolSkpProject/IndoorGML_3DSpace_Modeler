295c08fcf7178211ffa05f1927f704a57b7fef19
ulol <jeffk0408@naver.com>
2026-07-09 14:55:16 +0900

    Fix shared definition cell space conversion

--- indoor3d/ui/commands/conversion_message_formatter.rb
+++ indoor3d/ui/commands/conversion_message_formatter.rb
@@ -5,14 +5,46 @@ module ULOL
     module IndoorCore
       module ConversionMessageFormatter
         def self.group_label(group)
-          name = group.respond_to?(:name) ? group.name.to_s.strip : ''
-          id = group.respond_to?(:entityID) ? group.entityID : nil
+          return 'unknown group' if group.nil?
+
+          unless entity_valid_for_label?(group)
+            id = safe_entity_id(group)
+            return "deleted group (entity #{id})" unless id.nil?
+
+            return 'deleted group'
+          end
+
+          name = safe_name(group)
+          id = safe_entity_id(group)
           return "#{name} (entity #{id})" unless name.empty? || id.nil?
           return name unless name.empty?
           return "entity #{id}" unless id.nil?
 
           'unknown group'
         end
+
+        def self.entity_valid_for_label?(group)
+          return group.valid? if group.respond_to?(:valid?)
+
+          true
+        rescue StandardError
+          false
+        end
+        private_class_method :entity_valid_for_label?
+
+        def self.safe_name(group)
+          group.respond_to?(:name) ? group.name.to_s.strip : ''
+        rescue StandardError
+          ''
+        end
+        private_class_method :safe_name
+
+        def self.safe_entity_id(group)
+          group.respond_to?(:entityID) ? group.entityID : nil
+        rescue StandardError
+          nil
+        end
+        private_class_method :safe_entity_id
 
         def self.result_message(converted_count, errors)
           message = +"Succeed : #{converted_count}\nFailed : #{errors.length}"
