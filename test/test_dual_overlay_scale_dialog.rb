81d9ba3cc9099478e5a6774a54b8b7412af70528
ulol <jeffk0408@naver.com>
2026-07-10 11:15:48 +0900

    Fix overlay scale dialog close and geometry-only state visibility

--- test/test_dual_overlay_scale_dialog.rb
+++ test/test_dual_overlay_scale_dialog.rb
@@ -20,6 +20,12 @@ module Sketchup
   end
 end
 
+module UI
+  class HtmlDialog
+    STYLE_UTILITY = :utility unless const_defined?(:STYLE_UTILITY, false)
+  end
+end
+
 module ULOL
   module Indoor3DGmlModeler
     module IndoorCore
@@ -75,6 +81,43 @@ module ULOL
           assert_equal true, Sketchup.test_active_model.active_view.invalidated
         end
 
+        def test_close_dialog_callback_disposes_dialog
+          dialog = DualOverlayScaleDialog.new
+          fake_html_dialog = FakeHtmlDialog.new
+          with_fake_html_dialog(fake_html_dialog) do
+            dialog.show
+
+            fake_html_dialog.trigger_action('closeDialog')
+
+            assert_equal 1, fake_html_dialog.close_count
+            assert_nil dialog.instance_variable_get(:@dialog)
+          end
+        end
+
+        def test_window_x_close_disposes_dialog_like_close_button
+          dialog = DualOverlayScaleDialog.new
+          fake_html_dialog = FakeHtmlDialog.new
+          with_fake_html_dialog(fake_html_dialog) do
+            dialog.show
+
+            fake_html_dialog.trigger_closed
+
+            assert_nil dialog.instance_variable_get(:@dialog)
+          end
+        end
+
+        def test_slider_thumb_uses_icon_yellow
+          dialog = DualOverlayScaleDialog.new
+          html = dialog.send(:html)
+
+          assert_includes html, '--knob: #fab005;'
+          assert_includes html, 'accent-color: var(--knob);'
+          assert_includes html, '-webkit-appearance: none;'
+          assert_includes html, '--range-progress'
+          assert_includes html, '::-webkit-slider-thumb'
+          assert_includes html, '::-moz-range-thumb'
+        end
+
         private
 
         def stored_scale
@@ -93,6 +136,56 @@ module ULOL
               @invalidated = true
             end
           end.new
+        end
+
+        def with_fake_html_dialog(fake_dialog)
+          previous_new = UI::HtmlDialog.method(:new)
+          UI::HtmlDialog.define_singleton_method(:new) { |**_options| fake_dialog }
+          yield
+        ensure
+          UI::HtmlDialog.define_singleton_method(:new) { |**options| previous_new.call(**options) }
+        end
+
+        class FakeHtmlDialog
+          attr_reader :close_count
+
+          def initialize
+            @callbacks = {}
+            @close_count = 0
+            @visible = false
+          end
+
+          def set_html(_html); end
+
+          def show
+            @visible = true
+          end
+
+          def visible?
+            @visible == true
+          end
+
+          def close
+            @close_count += 1
+            @visible = false
+          end
+
+          def add_action_callback(name, &block)
+            @callbacks[name] = block
+          end
+
+          def set_on_closed(&block)
+            @on_closed = block
+          end
+
+          def trigger_action(name)
+            @callbacks.fetch(name).call(nil)
+          end
+
+          def trigger_closed
+            @visible = false
+            @on_closed.call
+          end
         end
       end
     end
