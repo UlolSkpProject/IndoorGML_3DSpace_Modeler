9034a29c8d9a1bbf828b660e50432f8b88cfc5bb
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-07-21 11:13:54 +0900

    정규화 복구와 동등성 보고 확장

--- indoor3d/application/local_vertex_normalizer/report_v2.rb
+++ indoor3d/application/local_vertex_normalizer/report_v2.rb
@@ -13,10 +13,16 @@ module ULOL
           target_collision_cleanup:,
           post_retriangulation_cleanup:,
           rebuilt_pre_repair_validation:,
+          forced_retriangulation:,
+          final_surface_equivalence:,
           final_repair:
         )
           report[:normalization_strategy] = :validated_triangle_rebuild_v2
           report[:axis_constraint_priority] = axis_plane_plan[:axis_priority]
+          report[:constrained_coordinate_count] =
+            axis_plane_plan[:constrained_coordinate_count]
+          report[:multi_axis_constrained_vertex_count] =
+            axis_plane_plan[:multi_axis_constrained_vertex_count]
           report[:resolved_axis_constraint_conflict_count] =
             axis_plane_plan[:resolved_constraint_conflict_count]
           report[:discarded_axis_constraint_count] =
@@ -25,8 +31,12 @@ module ULOL
           report[:merged_target_vertex_count] = vertex_metrics[:merged_target_vertex_count]
           report[:target_collision_samples] = vertex_metrics[:target_collisions]
           report[:target_collision_cleanup] = target_collision_cleanup
+          report[:forced_retriangulation] = forced_retriangulation
+          report[:forced_retriangulation_source_face_count] =
+            forced_retriangulation[:source_face_keys].length
           report[:post_retriangulation_cleanup] = post_retriangulation_cleanup
           report[:rebuilt_pre_repair_validation] = rebuilt_pre_repair_validation
+          report[:final_surface_equivalence] = final_surface_equivalence
           report[:final_entity_repair] = final_repair
           report[:surface_border_repair_count] =
             final_repair.dig(:surface_border, :repairs).to_i
@@ -36,7 +46,8 @@ module ULOL
             final_repair.dig(:stray_edges, :removed_edges).to_i
           report[:reverse_face_repair_count] =
             final_repair.dig(:reverse_faces, :reversed_faces).to_i
-          report[:manifold] = final_repair[:manifold]
+          report[:manifold] = final_repair[:manifold] &&
+            final_surface_equivalence[:equivalent]
           report
         end
       end
