3cc6f796d64512a45db14ca7a9e2b0b68309d3bf
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-07-31 01:13:22 +0900

    Bulk 결과 비모달 UI 정책 회귀 테스트 추가

--- test/test_ui_feedback_policy.rb
+++ test/test_ui_feedback_policy.rb
@@ -5,13 +5,21 @@ require 'minitest/autorun'
 class UiFeedbackPolicyTest < Minitest::Test
   ROOT = File.expand_path('..', __dir__)
 
-  def test_feedback_policy_uses_notification_and_zero_delay_event_turn
+  def test_feedback_policy_keeps_modal_helper_zero_delay_only
     source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
 
     assert_includes source, 'UI::Notification.new'
     assert_includes source, 'UI.start_timer(0, false)'
     assert_includes source, 'UI.stop_timer(timer_id)'
     refute_includes source, 'UI.start_timer(0.5'
+  end
+
+  def test_notification_failure_stays_non_modal
+    source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
+
+    assert_includes source, 'fallback_non_modal'
+    assert_includes source, 'Sketchup.status_text = message'
+    refute_match(/def notify\(message\).*defer_modal/m, source)
   end
 
   def test_notification_retention_does_not_attach_dismiss_button
@@ -21,13 +29,13 @@ class UiFeedbackPolicyTest < Minitest::Test
     refute_match(/notification\.on_dismiss\s*(?:do|\{)/, source)
   end
 
-  def test_cell_space_commands_do_not_show_modal_success_results_immediately
+  def test_cell_space_commands_never_schedule_post_bulk_modal_feedback
     source = File.read(File.join(ROOT, 'indoor3d/ui/commands/cell_space_commands.rb'))
 
     refute_includes source, 'UI.messagebox('
+    refute_includes source, 'UiFeedback.defer_modal'
     assert_includes source, 'UiFeedback.publish_result'
     assert_includes source, 'UiFeedback.notify'
-    assert_includes source, 'UiFeedback.defer_modal'
   end
 
   def test_toolbar_type_change_uses_one_batch_call
@@ -37,11 +45,12 @@ class UiFeedbackPolicyTest < Minitest::Test
     refute_match(/cell_space_groups\.each.*change_cell_space_type/m, source)
   end
 
-  def test_indoor_model_conversion_uses_shared_feedback_policy
+  def test_indoor_model_conversion_never_schedules_post_bulk_modal_feedback
     source = File.read(File.join(ROOT, 'indoor3d/application/indoor_model/ui_feedback.rb'))
 
     assert_includes source, 'UiFeedback.publish_result'
-    assert_includes source, 'UiFeedback.defer_modal'
+    assert_includes source, 'UiFeedback.notify'
+    refute_includes source, 'UiFeedback.defer_modal'
     refute_includes source, 'UI.messagebox('
   end
 end
