d6fb0c5f45503df4dbaef744bd23e92d17aeaf87
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-07-31 07:11:55 +0900

    UI 상시 디스패처 회귀 테스트 갱신

--- test/test_ui_feedback_policy.rb
+++ test/test_ui_feedback_policy.rb
@@ -5,13 +5,32 @@ require 'minitest/autorun'
 class UiFeedbackPolicyTest < Minitest::Test
   ROOT = File.expand_path('..', __dir__)
 
-  def test_feedback_policy_keeps_modal_helper_zero_delay_only
+  def test_feedback_policy_uses_one_persistent_dispatcher
     source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
 
-    assert_includes source, 'UI::Notification.new'
-    assert_includes source, 'UI.start_timer(0, false)'
-    assert_includes source, 'UI.stop_timer(timer_id)'
+    assert_includes source, 'UI.start_timer(DISPATCH_INTERVAL_SECONDS, true)'
+    assert_includes source, 'def start_dispatcher'
+    assert_includes source, 'def dispatch_pending_feedback'
+    assert_includes source, 'UiFeedback.start_dispatcher if defined?(UI)'
+    refute_includes source, 'UI.start_timer(0, false)'
     refute_includes source, 'UI.start_timer(0.5'
+  end
+
+  def test_feedback_calls_enqueue_instead_of_showing_ui_inline
+    source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
+
+    assert_match(/def notify\(message\)\s+enqueue_feedback\(:notification, message\)/m, source)
+    assert_match(/def defer_modal\(message, \*arguments\)\s+enqueue_feedback\(:modal, message, arguments\)/m, source)
+    assert_includes source, 'ready_after_tick:'
+    assert_includes source, 'return false if @dispatcher_tick <= item[:ready_after_tick].to_i'
+  end
+
+  def test_repeating_dispatcher_guards_modal_reentry
+    source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
+
+    assert_includes source, 'return false if @dispatching_modal == true'
+    assert_includes source, '@dispatching_modal = true'
+    assert_includes source, '@dispatching_modal = false'
   end
 
   def test_notification_failure_stays_non_modal
@@ -19,21 +38,13 @@ class UiFeedbackPolicyTest < Minitest::Test
 
     assert_includes source, 'fallback_non_modal'
     assert_includes source, 'Sketchup.status_text = message'
-    refute_match(/def notify\(message\).*defer_modal/m, source)
   end
 
-  def test_notification_retention_does_not_attach_dismiss_button
-    source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
-
-    assert_includes source, '@last_notification = notification'
-    refute_match(/notification\.on_dismiss\s*(?:do|\{)/, source)
-  end
-
-  def test_cell_space_commands_never_schedule_post_bulk_modal_feedback
+  def test_cell_space_commands_never_create_post_bulk_timers_or_messageboxes
     source = File.read(File.join(ROOT, 'indoor3d/ui/commands/cell_space_commands.rb'))
 
     refute_includes source, 'UI.messagebox('
-    refute_includes source, 'UiFeedback.defer_modal'
+    refute_includes source, 'UI.start_timer('
     assert_includes source, 'UiFeedback.publish_result'
     assert_includes source, 'UiFeedback.notify'
   end
@@ -45,12 +56,12 @@ class UiFeedbackPolicyTest < Minitest::Test
     refute_match(/cell_space_groups\.each.*change_cell_space_type/m, source)
   end
 
-  def test_indoor_model_conversion_never_schedules_post_bulk_modal_feedback
+  def test_indoor_model_conversion_never_creates_post_bulk_ui_timer
     source = File.read(File.join(ROOT, 'indoor3d/application/indoor_model/ui_feedback.rb'))
 
     assert_includes source, 'UiFeedback.publish_result'
     assert_includes source, 'UiFeedback.notify'
-    refute_includes source, 'UiFeedback.defer_modal'
+    refute_includes source, 'UI.start_timer('
     refute_includes source, 'UI.messagebox('
   end
 end
