cca2840569fe09b7c103ba810698ca7865db0f2b
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-07-31 00:15:55 +0900

    CellSpace UI 피드백 정책 회귀 테스트 추가

---
+++ test/test_ui_feedback_policy.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class UiFeedbackPolicyTest < Minitest::Test
+  ROOT = File.expand_path('..', __dir__)
+
+  def test_feedback_policy_uses_notification_and_zero_delay_event_turn
+    source = File.read(File.join(ROOT, 'indoor3d/ui/ui_feedback.rb'))
+
+    assert_includes source, 'UI::Notification.new'
+    assert_includes source, 'UI.start_timer(0, false)'
+    assert_includes source, 'UI.stop_timer(timer_id)'
+    refute_includes source, 'UI.start_timer(0.5'
+  end
+
+  def test_cell_space_commands_do_not_show_modal_success_results_immediately
+    source = File.read(File.join(ROOT, 'indoor3d/ui/commands/cell_space_commands.rb'))
+
+    refute_includes source, 'UI.messagebox('
+    assert_includes source, 'UiFeedback.publish_result'
+    assert_includes source, 'UiFeedback.notify'
+    assert_includes source, 'UiFeedback.defer_modal'
+  end
+
+  def test_toolbar_type_change_uses_one_batch_call
+    source = File.read(File.join(ROOT, 'indoor3d/ui/commands/cell_space_commands.rb'))
+
+    assert_includes source, 'indoor_model.change_cell_space_types('
+    refute_match(/cell_space_groups\.each.*change_cell_space_type/m, source)
+  end
+
+  def test_indoor_model_conversion_uses_shared_feedback_policy
+    source = File.read(File.join(ROOT, 'indoor3d/application/indoor_model/ui_feedback.rb'))
+
+    assert_includes source, 'UiFeedback.publish_result'
+    assert_includes source, 'UiFeedback.defer_modal'
+    refute_includes source, 'UI.messagebox('
+  end
+end
