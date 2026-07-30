61f32233c5e910304360e8c50bbf20fc05dd1eb4
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-07-31 00:19:28 +0900

    동적 HtmlDialog용 공통 drop guard 로더 추가

---
+++ indoor3d/ui/html_dialog_safety.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module ULOL
+  module Indoor3DGmlModeler
+    module IndoorCore
+      module HtmlDialogSafety
+        EXTERNAL_FILE_DROP_GUARD_PATH = File.join(
+          __dir__,
+          'html',
+          'shared',
+          'external_file_drop_guard.js'
+        ).freeze
+
+        class << self
+          def external_file_drop_guard_script
+            @external_file_drop_guard_script ||= File.read(
+              EXTERNAL_FILE_DROP_GUARD_PATH,
+              mode: 'r:BOM|UTF-8'
+            ).freeze
+          end
+
+          def external_file_drop_guard_script_tag
+            <<~HTML
+              <script>
+              #{external_file_drop_guard_script}
+              initExternalFileDropGuard();
+              </script>
+            HTML
+          end
+        end
+      end
+    end
+  end
+end
