df1e688848c61919e89ca72904b087da559ca800
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-01 00:04:39 +0900

    2D 점 포함 판정 회귀 범위 확대

--- test/test_geometry_polygon2d.rb
+++ test/test_geometry_polygon2d.rb
@@ -8,12 +8,44 @@ module ULOL
   module Indoor3DGmlModeler
     module Utils
       class GeometryPolygon2dTest < Minitest::Test
+        TOLERANCE = 0.001
+
         def test_point_in_polygon_2d_matches_boundary_and_interior_cases
           polygon = [[0.0, 0.0], [10.0, 0.0], [10.0, 10.0], [0.0, 10.0]]
 
-          assert Geometry.point_in_polygon_2d?([5.0, 5.0], polygon, 0.001)
-          assert Geometry.point_in_polygon_2d?([0.0, 5.0], polygon, 0.001)
-          refute Geometry.point_in_polygon_2d?([11.0, 5.0], polygon, 0.001)
+          assert Geometry.point_in_polygon_2d?([5.0, 5.0], polygon, TOLERANCE)
+          assert Geometry.point_in_polygon_2d?([0.0, 5.0], polygon, TOLERANCE)
+          assert Geometry.point_in_polygon_2d?([0.0, 0.0], polygon, TOLERANCE)
+          refute Geometry.point_in_polygon_2d?([11.0, 5.0], polygon, TOLERANCE)
+        end
+
+        def test_point_in_polygon_2d_is_independent_of_winding_order
+          polygon = [[0.0, 0.0], [10.0, 0.0], [10.0, 10.0], [0.0, 10.0]]
+
+          assert Geometry.point_in_polygon_2d?([5.0, 5.0], polygon, TOLERANCE)
+          assert Geometry.point_in_polygon_2d?([5.0, 5.0], polygon.reverse, TOLERANCE)
+          refute Geometry.point_in_polygon_2d?([15.0, 5.0], polygon, TOLERANCE)
+          refute Geometry.point_in_polygon_2d?([15.0, 5.0], polygon.reverse, TOLERANCE)
+        end
+
+        def test_point_in_polygon_2d_preserves_concave_region_classification
+          polygon = [
+            [0.0, 0.0],
+            [10.0, 0.0],
+            [10.0, 4.0],
+            [4.0, 4.0],
+            [4.0, 10.0],
+            [0.0, 10.0]
+          ]
+
+          assert Geometry.point_in_polygon_2d?([2.0, 8.0], polygon, TOLERANCE)
+          assert Geometry.point_in_polygon_2d?([8.0, 2.0], polygon, TOLERANCE)
+          refute Geometry.point_in_polygon_2d?([8.0, 8.0], polygon, TOLERANCE)
+          assert Geometry.point_in_polygon_2d?([4.0, 7.0], polygon, TOLERANCE)
+        end
+
+        def test_empty_polygon_is_outside
+          refute Geometry.point_in_polygon_2d?([0.0, 0.0], [], TOLERANCE)
         end
 
         def test_intersect_polygons_2d_and_area_value
