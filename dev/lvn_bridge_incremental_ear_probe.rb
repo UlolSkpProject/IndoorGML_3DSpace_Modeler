680f43fbf4f419276a3ea1b669478d61937fa278
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-02 01:09:28 +0900

    LVN incremental ear probe helper 설치 수정

---
+++ dev/lvn_bridge_incremental_ear_probe.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+probe_path = File.join(__dir__, 'lvn_bridge_nearest_first_probe.rb')
+source = File.binread(probe_path).force_encoding(Encoding::UTF_8)
+
+old_install = <<~'RUBY'
+        LocalVertexNormalizer.class_eval do
+          define_method(
+            :triangulate_exact_weak_polygon,
+            IncrementalEarTriangulation.instance_method(
+              :triangulate_exact_weak_polygon
+            )
+          )
+          private :triangulate_exact_weak_polygon
+        end
+RUBY
+
+new_install = <<~'RUBY'
+        incremental_method_names =
+          IncrementalEarTriangulation.private_instance_methods(false)
+        required_incremental_method_names = [
+          :triangulate_exact_weak_polygon,
+          :incremental_ear_reference_due?,
+          :incremental_ear_reference_selection,
+          :incremental_ear_refresh_candidate!,
+          :incremental_ear_heap_higher_priority?,
+          :incremental_ear_heap_push!,
+          :incremental_ear_heap_pop!
+        ]
+        missing_incremental_method_names =
+          required_incremental_method_names - incremental_method_names
+        unless missing_incremental_method_names.empty?
+          raise "Incremental ear probe helper methods missing: " \
+                "#{missing_incremental_method_names.inspect}"
+        end
+
+        LocalVertexNormalizer.class_eval do
+          incremental_method_names.each do |method_name|
+            define_method(
+              method_name,
+              IncrementalEarTriangulation.instance_method(method_name)
+            )
+            private method_name
+          end
+        end
+RUBY
+
+patched = source.sub(old_install, new_install)
+if patched == source
+  raise "Could not replace incremental ear installation block in #{probe_path}"
+end
+
+TOPLEVEL_BINDING.eval(patched, probe_path, 1)
