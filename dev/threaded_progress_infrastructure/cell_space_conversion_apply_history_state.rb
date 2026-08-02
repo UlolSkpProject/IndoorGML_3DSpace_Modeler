7d0d5bd6bedbf4fda3edea94b23920f57550086e
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-02 21:51:48 +0900

    CellSpace 적용 Undo Redo 상태 계약 추가

---
+++ dev/threaded_progress_infrastructure/cell_space_conversion_apply_history_state.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+module ULOL
+  module Indoor3DGmlModeler
+    module IndoorCore
+      module ThreadedProgressInfrastructure
+        class CellSpaceConversionApplyHistoryState
+          POSITIONS = %i[
+            not_applied
+            applied
+            undo_requested
+            undone
+            redo_requested
+          ].freeze
+
+          attr_reader :position
+
+          def initialize
+            reset!
+          end
+
+          def reset!
+            @position = :not_applied
+            true
+          end
+
+          def mark_applied!
+            @position = :applied
+            true
+          end
+
+          def mark_not_applied!
+            @position = :not_applied
+            true
+          end
+
+          def apply_recorded?
+            @position != :not_applied
+          end
+
+          def undo_allowed?
+            @position == :applied
+          end
+
+          def redo_allowed?
+            @position == :undone
+          end
+
+          def request_undo!
+            return false unless undo_allowed?
+
+            @position = :undo_requested
+            true
+          end
+
+          def confirm_undone!(matches:)
+            return matches == true if @position == :undone
+            return false unless @position == :undo_requested
+
+            if matches == true
+              @position = :undone
+              true
+            else
+              @position = :applied
+              false
+            end
+          end
+
+          def request_redo!
+            return false unless redo_allowed?
+
+            @position = :redo_requested
+            true
+          end
+
+          def confirm_redone!(matches:)
+            return matches == true if @position == :applied
+            return false unless @position == :redo_requested
+
+            if matches == true
+              @position = :applied
+              true
+            else
+              @position = :undone
+              false
+            end
+          end
+
+          def reject_undo_request!
+            return false unless @position == :undo_requested
+
+            @position = :applied
+            true
+          end
+
+          def reject_redo_request!
+            return false unless @position == :redo_requested
+
+            @position = :undone
+            true
+          end
+        end
+      end
+    end
+  end
+end
