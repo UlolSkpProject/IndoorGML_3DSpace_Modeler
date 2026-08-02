a3aacb8f00390d3af158310d9af142ce6639bbbe
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-02 22:43:03 +0900

    CellSpace 공식 Undo Redo API 계약 테스트 추가

---
+++ test/test_cell_space_conversion_direct_history_api.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+module ULOL
+  module Indoor3DGmlModeler
+    module IndoorCore
+      class CellSpaceConversionDirectHistoryApiTest < Minitest::Test
+        def test_direct_history_patch_uses_supported_undo_and_redo_methods
+          source = read_dev_file('cell_space_conversion_apply_direct_history_patch.rb')
+
+          assert_includes source, 'Sketchup.undo'
+          assert_includes source, 'Sketchup.redo'
+          refute_includes source, "Sketchup.send_action('editUndo:')"
+          refute_includes source, "Sketchup.send_action('editRedo:')"
+        end
+
+        def test_runner_loads_direct_history_patch
+          source = read_dev_file('run_cell_space_conversion_progress_apply.rb')
+
+          assert_includes source, "require_relative 'cell_space_conversion_apply_direct_history_patch'"
+          assert_includes source, 'Undo API: Sketchup.undo'
+        end
+
+        private
+
+        def read_dev_file(name)
+          File.read(
+            File.expand_path(
+              "../dev/threaded_progress_infrastructure/#{name}",
+              __dir__
+            )
+          )
+        end
+      end
+    end
+  end
+end
