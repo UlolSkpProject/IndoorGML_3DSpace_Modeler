10470f235534eb52b711944f3b4f8d035b59d291
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-02 23:33:49 +0900

    Rollback runner에 일반화 검증 정책 연결

--- dev/threaded_progress_infrastructure/run_cell_space_conversion_rollback_failure.rb
+++ dev/threaded_progress_infrastructure/run_cell_space_conversion_rollback_failure.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'cell_space_conversion_rollback_failure_runner'
+require_relative 'cell_space_conversion_rollback_verification_patch'
 
 ULOL::Indoor3DGmlModeler::IndoorCore::
   CellSpaceConversionRollbackFailureRunner.run!
