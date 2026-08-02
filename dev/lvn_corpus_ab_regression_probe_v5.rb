8c93ce8dfe1ebcc94e994c2aca1cadb3c7ab2baf
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-02 20:46:08 +0900

    LVN corpus A/B v5 진입점을 v7로 전환

--- dev/lvn_corpus_ab_regression_probe_v5.rb
+++ dev/lvn_corpus_ab_regression_probe_v5.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-# Deprecated compatibility entrypoint. Use the consolidated v6 launcher.
-load File.join(__dir__, 'lvn_corpus_ab_regression_probe_v6.rb')
+load File.join(__dir__, 'lvn_corpus_ab_regression_probe_v7.rb')
