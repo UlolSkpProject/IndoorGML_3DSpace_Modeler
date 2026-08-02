a87e842078dcd26412f82088528ca3a260454182
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-02 23:18:26 +0900

    CellSpace rollback 실패 주입 통합 실행 파일 추가

---
+++ dev/threaded_progress_infrastructure/run_cell_space_conversion_rollback_failure.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'cell_space_conversion_rollback_failure_runner'
+
+ULOL::Indoor3DGmlModeler::IndoorCore::
+  CellSpaceConversionRollbackFailureRunner.run!
