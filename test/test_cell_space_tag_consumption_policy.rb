8abe797e214f7cc75d51c9de58a15a146c3ddcbe
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-03 13:29:50 +0900

    Tag policy 테스트를 load-order 독립 구조로 분리

--- test/test_cell_space_tag_consumption_policy.rb
+++ test/test_cell_space_tag_consumption_policy.rb
@@ -1,31 +1,7 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-
-module ULOL
-  module Indoor3DGmlModeler
-    module IndoorCore
-      class CellSpaceLifecycleContext
-        attr_reader :calls
-
-        def initialize
-          @calls = []
-        end
-
-        def default_storey_name
-          'F01'
-        end
-
-        def initialize_scene(cell_space, storey: default_storey_name)
-          @calls << [:initialize_scene, cell_space, storey]
-          true
-        end
-      end
-    end
-  end
-end
-
-require_relative '../indoor3d/application/cell_space_behavior_policies'
+require_relative '../indoor3d/application/cell_space_auto_conversion_policy'
 
 module ULOL
   module Indoor3DGmlModeler
@@ -94,9 +70,7 @@ module ULOL
           end
         end
 
-        CellSpace = Struct.new(:sketchup_group)
-
-        def test_create_scene_consumes_only_outer_group_tag
+        def test_consumes_only_outer_group_tag
           model = Model.new
           nested = Entity.new(layer: Tag.new('F01F01_RM_DR'), model: model)
           outer = Entity.new(
@@ -104,48 +78,49 @@ module ULOL
             model: model,
             children: [nested]
           )
-          outer.set_attribute(
-            Policy::DICTIONARY_NAME,
-            Policy::AUTO_CONVERSION_DISABLED_KEY,
-            true
-          )
-          cell_space = CellSpace.new(outer)
-          context = CellSpaceLifecycleContext.new
 
-          assert context.initialize_scene(cell_space, storey: 'F01')
+          assert Policy.consume_tag!(outer)
 
           assert_equal 'Untagged', outer.layer.name
           assert_equal 1, outer.layer_assignments
           assert_equal 'F01F01_RM_DR', nested.layer.name
-          refute Policy.disabled?(outer)
-          assert_equal [[:initialize_scene, cell_space, 'F01']], context.calls
         end
 
         def test_already_untagged_group_does_not_write_layer_again
           model = Model.new
           outer = Entity.new(layer: model.layers[0], model: model)
-          context = CellSpaceLifecycleContext.new
 
-          assert context.initialize_scene(CellSpace.new(outer), storey: 'B01')
+          assert Policy.consume_tag!(outer)
 
           assert_equal 0, outer.layer_assignments
           assert_equal 'Untagged', outer.layer.name
         end
 
-        def test_tag_consumption_failure_aborts_before_base_scene_initialization
+        def test_missing_untagged_tag_returns_false_without_mutation
           outer = Entity.new(
             layer: Tag.new('F01F01_IP_RM_23'),
             model: Object.new
           )
-          context = CellSpaceLifecycleContext.new
 
-          error = assert_raises(RuntimeError) do
-            context.initialize_scene(CellSpace.new(outer), storey: 'F01')
-          end
+          refute Policy.consume_tag!(outer)
 
-          assert_equal 'CellSpace Tag could not be moved to Untagged', error.message
-          assert_empty context.calls
+          assert_equal 0, outer.layer_assignments
           assert_equal 'F01F01_IP_RM_23', outer.layer.name
+        end
+
+        def test_legacy_marker_can_be_cleared_after_tag_consumption
+          model = Model.new
+          outer = Entity.new(
+            layer: Tag.new('F01F01_IP_RM_23'),
+            model: model
+          )
+          assert Policy.disable!(outer)
+
+          assert Policy.consume_tag!(outer)
+          assert Policy.enable!(outer)
+
+          refute Policy.disabled?(outer)
+          assert_equal 'Untagged', outer.layer.name
         end
       end
     end
