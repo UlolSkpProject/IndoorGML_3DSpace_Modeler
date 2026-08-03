1130101e91a755179363775f74671424f120957f
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-03 10:35:02 +0900

    Adjacency overlay 정책 테스트 정리

--- test/test_cell_space_progress_overlay_policy.rb
+++ test/test_cell_space_progress_overlay_policy.rb
@@ -53,50 +53,6 @@ module ULOL
           false
         end
       end
-
-      class BulkCellSpaceConversionService
-        def initialize(
-          apply_guards:,
-          operation_runner:,
-          restore_active_path:,
-          rollback:,
-          logger:,
-          calls:
-        )
-          @apply_guards = apply_guards
-          @operation_runner = operation_runner
-          @restore_active_path = restore_active_path
-          @rollback = rollback
-          @logger = logger
-          @calls = calls
-        end
-
-        def call
-          result = @apply_guards.call do
-            @operation_runner.call(
-              'Bulk Convert',
-              rollback_if: proc { @rollback }
-            ) do
-              @calls << :work
-              :result
-            end
-          end
-
-          @calls << :runtime_restore if @rollback
-          safely_restore_active_path(success: !@rollback)
-          result
-        end
-
-        private
-
-        def safely_restore_active_path(success:)
-          @restore_active_path.call
-        rescue StandardError => e
-          context = success ? 'after commit' : 'during rollback'
-          @logger.puts("#{context}: #{e.message}")
-          nil
-        end
-      end
     end
   end
 end
@@ -108,12 +64,6 @@ module ULOL
     module IndoorCore
       module ProductionProgress
         class CellSpaceProgressOverlayPolicyTest < Minitest::Test
-          FakeLogger = Struct.new(:messages) do
-            def puts(message)
-              messages << message
-            end
-          end
-
           def test_snapshot_and_candidate_events_do_not_reach_overlay_sink
             service = AdjacencyService.new
             sink = Object.new
@@ -141,69 +91,6 @@ module ULOL
 
             assert_equal %i[start progress finish], service.events.map(&:first)
             assert service.events.all? { |event| event[1].equal?(sink) }
-          end
-
-          def test_success_restores_active_path_once_before_guard_exits
-            calls = []
-            service = build_service(calls, rollback: false)
-
-            result = service.call
-
-            assert_equal :result, result
-            assert_equal [
-              :guard_enter,
-              :operation_start,
-              :work,
-              :commit,
-              :restore_active_path,
-              :guard_exit
-            ], calls
-          end
-
-          def test_rollback_keeps_restore_after_guard_and_runtime_restore
-            calls = []
-            service = build_service(calls, rollback: true)
-
-            result = service.call
-
-            assert_equal :result, result
-            assert_equal [
-              :guard_enter,
-              :operation_start,
-              :work,
-              :abort,
-              :guard_exit,
-              :runtime_restore,
-              :restore_active_path
-            ], calls
-          end
-
-          private
-
-          def build_service(calls, rollback:)
-            logger = FakeLogger.new([])
-            apply_guards = proc do |&block|
-              calls << :guard_enter
-              result = block.call
-              calls << :guard_exit
-              result
-            end
-            operation_runner = proc do |_name, rollback_if:, &block|
-              calls << :operation_start
-              result = block.call
-              calls << (rollback_if.call ? :abort : :commit)
-              result
-            end
-            restore_active_path = proc { calls << :restore_active_path }
-
-            BulkCellSpaceConversionService.new(
-              apply_guards: apply_guards,
-              operation_runner: operation_runner,
-              restore_active_path: restore_active_path,
-              rollback: rollback,
-              logger: logger,
-              calls: calls
-            )
           end
         end
       end
