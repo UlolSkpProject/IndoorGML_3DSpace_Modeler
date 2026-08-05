02844ce5640fe6530a10ad58cd58109724e3ac6f
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2026-08-05 09:46:34 +0900

    정밀검사 report 메타데이터 패치를 로더에 연결

--- indoor3d/loader.rb
+++ indoor3d/loader.rb
@@ -3,3 +3,4 @@
 require_relative 'core'
 require_relative 'application/precision_validation/lvn_integration'
 require_relative 'application/precision_validation/validation_integration'
+require_relative 'application/precision_validation/report_metadata'
